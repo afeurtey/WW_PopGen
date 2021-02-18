e61738ebbe0ea1e05f6c8fd440a731fff2d63d63
afeurtey <alice.feurtey@gmail.com>
2021-02-18 13:37:18 +0100

    fix arrays

--- Assemble_array.sh
+++ Assemble_array.sh
@@ -4,10 +4,16 @@ source $1
 list=$2
 
 c=0;
-while read sample read1 read2 ; 
+#while read sample read1 read2 ; 
+while read  read_prefix sample ;
 do c=$((${c}+1)) ; 
 if [ "$c" -eq $SLURM_ARRAY_TASK_ID ] ; 
-then echo $c $sample $read1 $read2 ;
+then 
+
+read1=${read_prefix}_R1_001.fastq.gz
+read2=${read_prefix}_R2_001.fastq.gz
+
+echo $c $sample $read1 $read2 ;
 
 ${SPADES_PATH}spades.py  \
   -o ${DENOVO_ASSEMB}${sample} \
--- Depth_genes_estimates_array.sh
+++ Depth_genes_estimates_array.sh
@@ -2,18 +2,19 @@
 
 source $1
 sample_list=$2
+col_nb=$3
 
-
-sample=$(head -n $SLURM_ARRAY_TASK_ID $sample_list | tail -n1 | cut -f 2) 
+sample=$(head -n $SLURM_ARRAY_TASK_ID $sample_list | tail -n1 | cut -f $3) 
 echo $sample 
+temp_file=${pan_DP_gene}temp_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.txt
 
 #Per gene
 $BEDTOOLS_PATH coverage \
   -a $IPO323_GFF \
   -b ${mapped_dir}${sample}.bam \
   -d | \
-awk 'BEGIN{OFS = "\t"} {print $1 ";" $9, $10, $11} ' > temp${SLURM_ARRAY_TASK_ID}.txt
-$BEDTOOLS_PATH groupby -i temp${SLURM_ARRAY_TASK_ID}.txt -g 1 -c 3 -o median > \
+awk -v id="$sample" 'BEGIN{OFS = "\t"} {print id ";" $1 ";" $9, $10, $11} ' > ${temp_file}
+$BEDTOOLS_PATH groupby -i ${temp_file} -g 1 -c 3 -o median > \
   ${pan_DP_gene}${sample}.depth_per_gene.txt ;
-rm temp${SLURM_ARRAY_TASK_ID}.txt
+rm ${temp_file}
 echo "Genes are done!"
