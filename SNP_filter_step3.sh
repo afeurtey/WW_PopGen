8131b17d06a2ec8ad8b5fce59231952955076e8d
afeurtey <alice.feurtey@gmail.com>
2021-04-01 11:11:41 +0200

    Correct subsets and typos

--- SNP_filter_step3.sh
+++ SNP_filter_step3.sh
@@ -15,50 +15,57 @@ VCFNAME=${vcf_dir}${VCFBasename}.genotyped.ALL
 #  ---------------------------
 
 #Cleaning the file per pos and ind
-${GATK_PATH} IndexFeatureFile \
- -I ${VCFNAME}.filtered.clean.AB_filtered.variants.vcf.gz
+#${GATK_PATH} IndexFeatureFile \
+# -I ${VCFNAME}.filtered.clean.AB_filtered.variants.vcf.gz
 
-${GATK_PATH} SelectVariants  \
-  -R ${IPO323_REF} \
-  -V ${VCFNAME}.filtered.clean.AB_filtered.variants.vcf.gz \
-  --exclude-sample-name ${low_depth_samples_file} \
-  --exclude-sample-name ${clones_file}  \
-  --exclude-filtered \
-  --exclude-non-variants --remove-unused-alternates \
-  -O ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.vcf
-  
+#${GATK_PATH} SelectVariants  \
+#  -R ${IPO323_REF} \
+#  -V ${VCFNAME}.filtered.clean.AB_filtered.variants.vcf.gz \
+#  --exclude-sample-name ${low_depth_samples_file} \
+#  --exclude-sample-name ${clones_file}  \
+#  --exclude-filtered \
+#  --exclude-non-variants --remove-unused-alternates \
+#  -O ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.vcf
 
-gzip -f ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.vcf
+
+#gzip -f ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.vcf
+
 
 # Transform in tab file without genotypes to get stats
 ${BCFTOOLS_PATH} query \
   -f '%CHROM\t%POS\t%REF\t%ALT\n' \
-  ${vcf_dir}${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.vcf.gz \
-  > ${vcf_qual_check_dir}${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.tab
+  ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.vcf.gz \
+  > ${vcf_qual_check_dir}${VCFBasename}.genotyped.ALL.filtered.clean.AB_filtered.variants.good_samples.tab
 
 # Allele counts
 ${VCFTOOLS_PATH} \
-   --gzvcf ${vcf_dir}${VCFBasename}.genotyped.${CHR}.filtered.clean.AB_filtered.variants.good_samples.vcf.gz \
+   --gzvcf ${vcf_dir}${VCFBasename}.genotyped.ALL.filtered.clean.AB_filtered.variants.good_samples.vcf.gz \
    --counts2 \
-   --out ${vcf_qual_check_dir}${VCFBasename}.genotyped.${CHR}.filtered.clean.AB_filtered.variants.good_samples
+   --out ${vcf_qual_check_dir}${VCFBasename}.genotyped.ALL.filtered.clean.AB_filtered.variants.good_samples
 
 
 #   -----------------------------------
 # | Create additional, subset VCF files |
 #   -----------------------------------
 
-# for diversity analyses: SNPs only, max-missing 80%
+# for diversity analyses: biallelic SNPs only
 ${VCFTOOLS_PATH} --gzvcf ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.vcf.gz \
         --recode --recode-INFO-all --remove-filtered-all --remove-indels \
-        --max-missing 0.8 \
-        --out ${VCFNAME}.filtered.clean.good_samples.SNP.max-m-0.8
+        --min-alleles 2 --max-alleles 2 \
+        --out ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.biall_SNP
 
-# for population genetics: SNPs only, max-missing, 5% MAF, thinned to 1 per 1000 bp
-${VCFTOOLS_PATH} --gzvcf ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.vcf.gz \
+# for diversity analyses: biallelic SNPs only, max-missing 80%
+${VCFTOOLS_PATH} --gzvcf ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.biall_SNP.vcf.gz \
+        --recode --recode-INFO-all --remove-filtered-all --remove-indels \
+        --min-alleles 2 --max-alleles 2 \
+        --max-missing 0.8 \
+        --out ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.biall_SNP.max-m-80
+
+# for population genetics: biallelic SNPs only, max-missing 100, 5% MAF, thinned to 1 per 1000 bp
+${VCFTOOLS_PATH} --gzvcf ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.biall_SNP.max-m-80.recode.vcf \
         --recode --recode-INFO-all --remove-filtered-all --remove-indels \
         --mac 1 --thin 1000 \
-        --max-missing 0.8 \
+        --max-missing 1 \
+        --min-alleles 2 --max-alleles 2 \
         --maf 0.05 \
-        --out ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.SNP.max-m-0.8.maf-0.05.thin-1000bp
-  
-
+        --out ${VCFNAME}.filtered.clean.AB_filtered.variants.good_samples.biall_SNP.max-m-1.maf-0.05.thin-1000bp
