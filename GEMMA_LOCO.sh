d58845f0b2c410fa87de88696abecbef7893c5fe
afeurtey <alice.feurtey@gmail.com>
2021-04-07 08:32:53 +0200

    Update GEMMA paths to new SNPcalling

--- GEMMA_LOCO.sh
+++ GEMMA_LOCO.sh
@@ -10,45 +10,47 @@ else
 fi
 
 echo "Working on chromosome" $CHR ;
-VCFNAME=${vcf_dir}${VCFBasename}.genotyped.${CHR}
+VCFNAME=${VCFBasename}.genotyped.ALL.filtered.clean.AB_filtered.variants.good_samples.max-m-80
+PERCHR_VCFNAME=${VCFBasename}.genotyped.${CHR}.filtered.clean.AB_filtered.variants.good_samples.max-m-80
 Pheno_fam_file=${GEA_dir}Phenotypes_with_climatic_variables.fam
 
 # Kinship matrix
-#${VCFTOOLS_PATH} \
-#  --gzvcf ${vcf_dir}${VCFBasename}.genotyped.ALL.filtered.clean.good_samples.SNP.max-m-0.8.maf-0.05.thin-1000bp.recode.vcf.gz \
-#  --recode --recode-INFO-all \
-#  --not-chr ${CHR} \
-#  --out ${GEA_dir}${VCFBasename}.genotyped.ALL.filtered.clean.good_samples.SNP.max-m-0.8.maf-0.05.thin-1000bp.wo_${CHR}
+${VCFTOOLS_PATH} \
+  --gzvcf ${vcf_dir}${VCFNAME}.vcf.gz \
+  --recode --recode-INFO-all \
+  --not-chr ${CHR} \
+  --thin 200 \
+  --out ${GEA_dir}${VCFNAME}.thin-200bp.wo_${CHR}
 
-#${SOFTPATH}plink \
-#  --vcf ${GEA_dir}${VCFBasename}.genotyped.ALL.filtered.clean.good_samples.SNP.max-m-0.8.maf-0.05.thin-1000bp.wo_${CHR}.recode.vcf \
-#  --make-bed \
-#  --out ${GEA_dir}For_kinship_${CHR} \
-#  --double-id 
+${SOFTPATH}plink \
+  --vcf ${GEA_dir}${VCFNAME}.thin-200bp.wo_${CHR}.recode.vcf \
+  --make-bed \
+  --out ${GEA_dir}For_kinship_${CHR} \
+  --double-id 
 
 cd ${GEA_dir}
 cp ${Pheno_fam_file} For_kinship_${CHR}.fam
 
-#gemma \
-#  -bfile For_kinship_${CHR} \
-#  -gk 1 \
-#  -o Kinship_for_chr${CHR}
+gemma \
+  -bfile For_kinship_${CHR} \
+  -gk 1 \
+  -o Kinship_for_chr${CHR}
 
 
 # Association
 
 ${SOFTPATH}plink \
-  --vcf ${vcf_dir}${VCFBasename}.genotyped.${CHR}.filtered.clean.good_samples.SNP.max-m-0.8.recode.vcf.gz \
+  --vcf ${vcf_dir}${PERCHR_VCFNAME}.recode.vcf.gz \
   --make-bed \
-  --out ${GEA_dir}${VCFBasename}.genotyped.${CHR}.filtered.clean.good_samples.SNP.max-m-0.8 \
+  --out ${GEA_dir}${PERCHR_VCFNAME} \
   --double-id 
 
-cp ${Pheno_fam_file} ${GEA_dir}${VCFBasename}.genotyped.${CHR}.filtered.clean.good_samples.SNP.max-m-0.8.fam
+cp ${Pheno_fam_file} ${GEA_dir}${PERCHR_VCFNAME}.fam
 
 for i in {1..19} ;
 do
 gemma \
-  -bfile ${GEA_dir}${VCFBasename}.genotyped.${CHR}.filtered.clean.good_samples.SNP.max-m-0.8 \
+  -bfile ${GEA_dir}${PERCHR_VCFNAME} \
   -lmm 1 \
   -k output/Kinship_for_chr${CHR}.cXX.txt \
   -o Kinship_for_chr${CHR}_for_phenotype_${i} \
