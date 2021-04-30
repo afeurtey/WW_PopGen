a44607110a09b050dcd6545cb7460a43c7f1573c
afeurtey <alice.feurtey@gmail.com>
2021-04-30 14:52:26 +0200

    add TajD

--- Diversity_with_python.py
+++ Diversity_with_python.py
@@ -1,5 +1,6 @@
 #/usr/bin/python3
 
+import sys
 import argparse
 import allel
 import numpy as np
@@ -25,7 +26,16 @@ parser.add_argument('--vcf_file', type=str, help='path to the input vcf file.')
 parser.add_argument('--bed_file', type=str, help='path to the input bed file containing regions to compute the diversity over')
 parser.add_argument('--sample_list', type=str, help='path to the input sample list. Extension must be args.')
 parser.add_argument('--out_dir', type=str, help='path to the output directory')
+parser.add_argument('--pi', action="store_true")
+parser.add_argument('--TajD', action="store_true")
 A = parser.parse_args()
+
+
+
+if not A.pi :
+    if not A.TajD :
+        sys.exit("\n\n I have to stop now! Please use the options --pi and/or --TajD. ")
+
 
 
 #  ------------
@@ -68,13 +78,18 @@ for chromosome in [x+1 for x in range(21)] :
     for row in dict_bed[str(chromosome)] :
         start = row[1]
         stop = row[2]
+        to_write = [subset_name, str(chromosome), str(start), str(stop)] 
         try :
-            pi = allel.sequence_diversity(pos, ac, start=int(start), stop=int(stop))
-            to_write = [subset_name, str(chromosome), str(start), str(stop), str(round(pi, 6))]
+            if A.pi :
+                pi = allel.sequence_diversity(pos, ac, start = int(start), stop = int(stop))
+                to_write.append(str(round(pi, 6)))
+            elif A.TajD :
+                taj_D = allel.tajima_d(ac, pos, start = int(start), stop = int(stop))
+                to_write.append(str(round(taj_D, 6)))
+
             shutup = out.write("\t".join(to_write) + "\n")
         except :
             print([subset_name, str(chromosome), str(start), str(stop)])
-    print(c)
 
 
 print("All done!")
