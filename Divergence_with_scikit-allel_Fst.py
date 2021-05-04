a5fc1b0d80b6a03e4707b27a4d23d35b9739f230
afeurtey <alice.feurtey@gmail.com>
2021-05-04 20:58:26 +0200

    Add Fst divergence and fix dxy bugs

--- Divergence_with_scikit-allel_Fst.py
+++ Divergence_with_scikit-allel_Fst.py
@@ -3,6 +3,7 @@
 import sys
 import argparse
 import allel
+import numpy as np
 
 
 #    <<>><<>><<>><<>><<>><<>><<>><<>><<>>
@@ -16,15 +17,8 @@ parser.add_argument('--bed_file', type=str, help='path to the input bed file con
 parser.add_argument('--sample_list1', type=str, help='path to the first sample list. Extension must be args.')
 parser.add_argument('--sample_list2', type=str, help='path to the second sample list. Extension must be args.')
 parser.add_argument('--out_dir', type=str, help='path to the output directory')
-parser.add_argument('--dxy', action="store_true")
-parser.add_argument('--Fst', action="store_true")
+
 A = parser.parse_args()
-
-
-
-if not A.dxy :
-    if not A.Fst :
-        sys.exit("\n\n I have to stop now! Please use the options --dxy and/or --Fst. ")
 
 
 
@@ -34,7 +28,7 @@ if not A.dxy :
 
 # Derive info from the subset file name
 subset1_name = A.sample_list1.replace(".args", "").rsplit("/", 1)[1]
-subset2_name = A.sample_list1=2.replace(".args", "").rsplit("/", 1)[1]
+subset2_name = A.sample_list2.replace(".args", "").rsplit("/", 1)[1]
 print("\n" + subset1_name + " vs " + subset2_name)
 
 
@@ -48,7 +42,8 @@ with open(A.bed_file, "r") as bed_input :
             dict_bed[str(row[0])] = []
         dict_bed[str(row[0])].append(row)
 
-out = open(A.out_dir + subset_name + "_divergence." + subset1_name + "_vs_" + subset2_name+ ".tsv", "w")
+out = open(A.out_dir + "Divergence.Fst." + subset1_name + "_vs_" + subset2_name+ ".tsv", "w")
+out.write("\t".join(["Subset1", "Subset2", "Chromosome", "Hudsons_Fst", "Weir_Cockerham_Fst"]) + "\n")
 
 # Read samples from the two populations as lists
 with open(A.sample_list1, "r") as input_file :
@@ -59,30 +54,27 @@ with open(A.sample_list2, "r") as input_file :
 
 # Loop over chromosomes
 for chromosome in [x+1 for x in range(21)] :
-    print(subset1_name, subset2_name, str(int(chromosome)))
+    print(subset1_name, subset2_name, str(int(chromosome)), "\n")
 
     # Reading the SNPs for the right chromosome and the right samples
-    callset = allel.read_vcf(A.vcf_file, samples = subset1 + subset2, region = str(chromosome))
-    #callset.keys()
-    sample_list = callset['samples']
-    pos = callset['variants/POS'][:]
-    h = allel.GenotypeArray(callset['calldata/GT'])
+    callset = allel.read_vcf(A.vcf_file, samples = subset1 + subset2, region = str(int(chromosome)))
+    if callset:
+        sample_list = callset['samples']
+        pos = callset['variants/POS'][:]
+        h = allel.GenotypeArray(callset['calldata/GT'])
+        to_write = [subset1_name, subset2_name, str(chromosome)]
 
-    ac1 = h.count_alleles(subpop=[sample_list.index(x) for x in subset1])
-    ac2 = h.count_alleles(subpop=[sample_list.index(x) for x in subset2])
-    for row in dict_bed[str(chromosome)] :
-        start = row[1]
-        stop = row[2]
-        to_write = [subset1_name, subset2_name, str(chromosome), str(start), str(stop)]
-        try :
-            if A.dxy :
-                dxy = sequence_divergence(pos, ac1, ac2, start=start, stop=stop)
-                to_write.append(str(round(dxy, 6)))
+        ac1 = h.count_alleles(subpop=[list(sample_list).index(x) for x in subset1])
+        ac2 = h.count_alleles(subpop=[list(sample_list).index(x) for x in subset2])
+        num, den = allel.hudson_fst(ac1, ac2)
+        fst = np.sum(num) / np.sum(den)
+        to_write.append(str(fst))
 
-            shutup = out.write("\t".join(to_write) + "\n")
-        except :
-            print([subset_name, str(chromosome), str(start), str(stop)])
+        a, b, c = allel.weir_cockerham_fst(h, [[list(sample_list).index(x) for x in subset1], [list(sample_list).index(x) for x in subset2]])
+        fst = np.sum(a) / (np.sum(a) + np.sum(b) + np.sum(c))
+        to_write.append(str(fst))
 
+        out.write("\t".join(to_write) + "\n")
 
 print("All done!")
 out.close()
